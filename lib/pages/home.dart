6deed81496ef4b2b087b3ee4d0fdad9324e008a9
Hong Phuc <hongphuc.dthp@gmail.com>
2023-09-24 12:52:45 +0700

    flutter: create appBar

---
+++ lib/pages/home.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar(),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text('Breakfast TUTORIAL',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          )),
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      centerTitle: true,
+      leading: GestureDetector(
+          onTap: () {},
+          child: Container(
+              margin: const EdgeInsets.all(10),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: const Color(0xffF7F8F8),
+              ),
+              child: SvgPicture.asset(
+                'assets/icons/Arrow - Left 2.svg',
+                width: 20,
+                height: 20,
+              ))),
+      actions: [
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            width: 37,
+            margin: const EdgeInsets.all(10),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: const Color(0xffF7F8F8),
+            ),
+            child: SvgPicture.asset(
+              'assets/icons/dots.svg',
+              width: 5,
+              height: 5,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
