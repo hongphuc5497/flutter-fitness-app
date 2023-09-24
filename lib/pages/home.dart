0192f3a8b6ec534d3f7c13ec479a4be73ac8274a
Hong Phuc <hongphuc.dthp@gmail.com>
2023-09-24 13:58:09 +0700

    flutter: create searchField

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
+import 'package:fitness/widgets/appBar.dart';
+import 'package:fitness/widgets/searchField.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -8,53 +11,10 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
-    );
-  }
-
-  AppBar appBar() {
-    return AppBar(
-      title: const Text('Breakfast TUTORIAL',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-          )),
       backgroundColor: Colors.white,
-      elevation: 0.0,
-      centerTitle: true,
-      leading: GestureDetector(
-          onTap: () {},
-          child: Container(
-              margin: const EdgeInsets.all(10),
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: const Color(0xffF7F8F8),
-              ),
-              child: SvgPicture.asset(
-                'assets/icons/Arrow - Left 2.svg',
-                width: 20,
-                height: 20,
-              ))),
-      actions: [
-        GestureDetector(
-          onTap: () {},
-          child: Container(
-            width: 37,
-            margin: const EdgeInsets.all(10),
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: const Color(0xffF7F8F8),
-            ),
-            child: SvgPicture.asset(
-              'assets/icons/dots.svg',
-              width: 5,
-              height: 5,
-            ),
-          ),
-        )
-      ],
+      body: Column(children: [
+        searchField(),
+      ]),
     );
   }
 }
---
+++ lib/widgets/searchField.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+Container searchField() {
+  return Container(
+    margin: const EdgeInsets.only(top: 40, right: 20, left: 20),
+    decoration: const BoxDecoration(
+      boxShadow: [
+        BoxShadow(
+          // color: Color(0xff1D1617).withOpacity(0.11),
+          color: Color.fromRGBO(29, 22, 23, 0.11),
+          blurRadius: 40,
+          spreadRadius: 0.0,
+        )
+      ],
+    ),
+    child: TextField(
+      decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
+          contentPadding: const EdgeInsets.all(15),
+          hintText: 'Search Pancake',
+          hintStyle: const TextStyle(
+            color: Color(0xffDDDADA),
+            fontSize: 14,
+          ),
+          prefixIcon: Padding(
+              padding: const EdgeInsets.all(12),
+              child: SvgPicture.asset('assets/icons/Search.svg')),
+          suffixIcon: Container(
+            width: 100,
+            child: IntrinsicHeight(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  const VerticalDivider(
+                    color: Colors.black,
+                    indent: 10,
+                    endIndent: 10,
+                    thickness: 0.1,
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SvgPicture.asset('assets/icons/Filter.svg')),
+                ],
+              ),
+            ),
+          ),
+          border: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(15)),
+            borderSide: BorderSide.none,
+          )),
+    ),
+  );
+}
